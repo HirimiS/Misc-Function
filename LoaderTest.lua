1cae70747ffef44879229e63f67dd4c83b72910b
HirimiS <142788316+HirimiS@users.noreply.github.com>
2024-01-14 19:43:03 +0700

    Update LoaderTest.lua

--- LoaderTest.lua
+++ LoaderTest.lua
@@ -1,4 +1,4 @@
---Hirimi Hub Hyper - Rewrite Fixed & Update #9.1
+--Hirimi Hub Hyper - Rewrite Fixed & Update #9.2
 repeat wait() until game:IsLoaded()
 notis = require(game.ReplicatedStorage:WaitForChild("Notification"))
 notis.new("<Color=White>HIRIMI HUB HYPER<Color=/>"):Display()
@@ -5037,7 +5037,7 @@ if Zou then
             if KillTrials then
                 for i,v in pairs(WS.Characters:GetChildren()) do
                     magnitude = (LP.Character.HumanoidRootPart.Position - v.HumanoidRootPart.Position).Magnitude
-                    if magnitude <= 300 and v ~= LP.Character.HumanoidRootPart then
+                    if magnitude <= 300 and v ~= game.PLayers.LocalPlayer then
                         repeat task.wait()
                             if SpamSkillAllWeapon then
                                 chodienspamhirimixienchetcuchungmay = true
